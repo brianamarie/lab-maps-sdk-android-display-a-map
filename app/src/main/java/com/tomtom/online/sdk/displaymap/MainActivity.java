0fe5f7ab17a90b9e07cc2f01b3439ad76d950802
Briana Swift <brianamarie@github.com>
2019-09-11 10:47:19 +0200

    add onmapreadycallback

--- app/src/main/java/com/tomtom/online/sdk/displaymap/MainActivity.java
+++ app/src/main/java/com/tomtom/online/sdk/displaymap/MainActivity.java
@@ -1,16 +1,13 @@
 package com.tomtom.online.sdk.displaymap;
 
 import com.tomtom.online.sdk.map.TomtomMap;
-// Add here the OnMapReadyCallback import
+import com.tomtom.online.sdk.map.OnMapReadyCallback;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-// Add here the NonNull import
-
-
+import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity
-// Implement here the MainActivity with OnMapReadyCallback
-{
+implements OnMapReadyCallback {
   private TomtomMap tomtomMap;
 
   @Override
@@ -19,5 +16,8 @@ public class MainActivity extends AppCompatActivity
     setContentView(R.layout.activity_main);
   }
 
-  // Add here the onMapReady callback
+  @Override
+  public void onMapReady(@NonNull final TomtomMap tomtomMap) {
+    this.tomtomMap = tomtomMap;
+}
 }
